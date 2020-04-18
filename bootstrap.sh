f70e86e644b9173ac14d3a75ca7f84a560921fed
Lance Wicks <lw@judocoach.com>
2020-04-18 15:17:04 +0100

    Bump ubuntu and make bootstrap work
    Initial attempt at getting tapiriik working locally via vagrant.
    Bumping Ubuntu and mongodb to get the ball rolling
    
    This commit, SHOULD, be enough to get it running

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,19 +6,26 @@
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+
 
 sudo apt-get update
-sudo apt-get install -y python3-pip libxml2-dev libxslt-dev zlib1g-dev git redis-server rabbitmq-server mongodb-org=2.6.9
+sudo apt-get install -y python3-pip libxml2-dev libxslt-dev zlib1g-dev git redis-server rabbitmq-server mongodb-org
+
+sudo systemctl enable mongod
+sudo systemctl start mongod
+
 
 # Fix pip
 pip3 install --upgrade pip
 
 # Install app requirements
-pip install --upgrade -r /vagrant/requirements.txt
+pip3 install --upgrade -r /vagrant/requirements.txt
 
 # Fix the default python instance
 update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1
-update-alternatives --install /usr/bin/python python /usr/bin/python3.4 2
+update-alternatives --install /usr/bin/python python /usr/bin/python3.6 2
 
 # Put in a default local_settings.py (if one doesn't exist)
 if [ ! -f /vagrant/tapiriik/local_settings.py ]; then
@@ -26,4 +33,5 @@ if [ ! -f /vagrant/tapiriik/local_settings.py ]; then
     # Generate credential storage keys
     python /vagrant/credentialstore_keygen.py >> /vagrant/tapiriik/local_settings.py
 fi
+
 
